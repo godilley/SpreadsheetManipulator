711eedefb7003ca8792018593d423dd29d80f2a3
George Dilley <georgedilley@me.com>
2020-07-20 22:20:06 +0100

    Composer autoloading

--- app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-class App {
-    public function __construct() {
-        
-    }
-}
---
+++ src/SpreadsheetManipulator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app;
+
+class SpreadsheetManipulator
+{
+    /**
+     * SpreadsheetManipulator constructor.
+     * @param array $args CLI parameters
+     */
+    public function __construct(array $args)
+    {
+        echo 'Hello World';
+    }
+}
